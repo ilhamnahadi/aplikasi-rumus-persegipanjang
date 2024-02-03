972d2f4b029c36c989805201d1b2149a2299839f
ilhamnahadi <ilhamnahadi@gmail.com>
2024-02-03 21:18:37 +0700

    penambahan rumus persegi panjang

---
+++ persegipanjang.js
@@ -0,0 +1,14 @@
+function TentukanLuasDanKelilingPersegiPanjang(p, l) {
+    const panjang = p;
+    const lebar = l;
+    const luas = panjang * lebar;
+    const keliling = 2 * (panjang + lebar);
+  
+    console.log("Panjang=", panjang , "Lebar=", lebar);
+    console.log("Luas Persegi Panjang =", luas);
+    console.log("Keliling Persegi Panjang=", keliling);
+  
+    // Akhir pembuatan code
+  }
+  
+  TentukanLuasDanKelilingPersegiPanjang(5, 7);
